442afd8db45b41d93565e6193e5bdec791641b9b
Artur Sattelmaier <dubwoof@me.com>
2024-02-14 18:22:16 +0100

    go home link

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,12 @@
 import { JSX } from 'react';
 import { PageProps } from '../Page.types';
+import { Link } from 'react-router-dom';
 
 export function NotFound({}: PageProps): JSX.Element {
   return (
     <div>
       <h1>Not Found</h1>
+      <Link to="/">Go home</Link>
     </div>
   );
 }

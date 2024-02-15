29b1f31310cc88a619064a83bedc139d26cc0787
Artur Sattelmaier <dubwoof@me.com>
2024-02-15 11:31:19 +0100

    change color of nav

--- src/elements/Navigation/Navigation.styles.ts
+++ src/elements/Navigation/Navigation.styles.ts
@@ -1,6 +1,6 @@
 export const styles = {
-    // todo: slate-700 should be a global variable
-    nav: `fixed h-12 md:h-20 z-50 left-0 top-0 flex w-full justify-between content-center bg-gradient-to-b from-slate-700 backdrop-blur-2xl`,
+    // todo: slate-800 should be a global variable from root background
+    nav: `fixed h-12 md:h-20 z-50 left-0 top-0 flex w-full justify-between content-center bg-gradient-to-b from-slate-800 backdrop-blur-2xl`,
     navLine: `w-[1100px] flex justify-between items-center mx-auto px-4`,
     navLinks: 'hidden gap-8 md:flex',
     iconWrapper: `p-2 rounded-full hover:bg-gray-200 hover:bg-opacity-50 cursor-pointer active:bg-gray-200 active:bg-opacity-50`,

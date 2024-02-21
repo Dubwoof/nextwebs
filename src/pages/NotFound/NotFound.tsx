24abfc041211b60f5bd4936a809977a4e3462dd3
Artur Sattelmaier <dubwoof@me.com>
2024-02-21 21:43:11 +0100

    not found page style

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,18 @@
 import { JSX } from 'react';
 import { PageProps } from '../Page.types';
 import { Link } from 'react-router-dom';
+import { Section } from '../../components/Section/Section';
+import { NavWidth } from '../../layouts/NavWidth';
 
 export function NotFound({}: PageProps): JSX.Element {
   return (
-    <div>
-      <h1>Not Found</h1>
-      <Link to="/">Go home</Link>
-    </div>
+    <Section>
+      <NavWidth>
+        <div className="flex flex-col text-center h-[60vh] pt-16">
+          <h1>Not Found</h1>
+          <Link to="/">Go home</Link>
+        </div>
+      </NavWidth>
+    </Section>
   );
 }

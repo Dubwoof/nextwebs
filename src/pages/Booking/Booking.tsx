5f3380890100a636d4304ac24d44bf90cfbdd815
Artur Sattelmaier <dubwoof@me.com>
2024-03-31 20:06:10 +0200

    persons

--- src/pages/Booking/Booking.tsx
+++ src/pages/Booking/Booking.tsx
@@ -1,8 +1,11 @@
 import { PageProps } from '../Page.types';
 import { Typography } from '../../elements/Typography/Typography';
 import munich from '../../assets/munich.avif';
+import { useState } from 'react';
 
 export function Booking({}: PageProps): JSX.Element {
+  const [persons, setPersons] = useState(1);
+
   return (
     <div className="pb-16 lg:pt-32">
       <div
@@ -37,9 +40,16 @@ export function Booking({}: PageProps): JSX.Element {
           </div>
 
           <div className="flex gap-4 mb-4">
-            <div className="flex bg-white cursor-pointer h-16 aspect-square rounded-lg justify-center items-center p-2 text-background">14:00</div>
+            <div className="flex bg-white cursor-pointer h-16 aspect-square rounded-lg justify-center items-center p-2 text-background">
+              14:00
+            </div>
             <div className="flex bg-warning cursor-pointer h-16 aspect-square rounded-lg justify-center items-center p-2">17:00</div>
           </div>
+
+          <Typography variant="body1" className="text-bold text-xl mb-1">
+            Persons
+          </Typography>
+          <input className="h-16 w-16 rounded-lg mb-4 text-background text-center text-xl" name="persons" value={persons} type="number" />
 
           <div className="flex justify-center items-center bg-warning h-16 rounded-xl mb-1">Book free</div>
           <Typography variant="caption" className="font-robotoRegular text-xl mb-4 text-center">

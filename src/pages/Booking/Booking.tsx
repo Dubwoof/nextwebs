988be504c8b706e212f1551ee7dcc04aed947323
Artur Sattelmaier <dubwoof@me.com>
2024-03-31 19:23:06 +0200

    fix image maybe

--- src/pages/Booking/Booking.tsx
+++ src/pages/Booking/Booking.tsx
@@ -1,10 +1,14 @@
 import { PageProps } from '../Page.types';
 import { Typography } from '../../elements/Typography/Typography';
+import munich from '../../assets/munich.avif';
 
 export function Booking({}: PageProps): JSX.Element {
   return (
     <div className="pb-16 lg:pt-32">
-      <div className="bg-munich bg-center h-screen max-h-screen max-w-screen bg-cover w-screen flex flex-col justify-between px-4 pt-8">
+      <div
+        style={{ backgroundImage: `url(${munich})` }} // Inline style with background image
+        className="bg-munich bg-center h-svh bg-cover w-screen flex flex-col justify-between px-4 pt-8"
+      >
         <Typography variant="h1" className="font-robotoRegular text-4xl leading-8">
           Munich
           <br />
